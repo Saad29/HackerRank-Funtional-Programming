ebbbb0b58224f84125022430c2b2043efce6dfed
Saad <saadbin.sajjad@stud.tu-darmstadt.de>
2018-11-07 00:08:19 +0100

    Added a recursive and pattern matched solution and other solutions taken from discussions in comments

--- hrank/src/test/scala/HelloWorldNTimes.scala
+++ hrank/src/test/scala/HelloWorldNTimes.scala
@@ -1,17 +1,43 @@
 object HelloWorldNTimes extends App
 {
 
+//First Method ( my Naive solution)
 
-  def f(n: Int) =
-  {
-    var m = n
-    do
+//  def f(n: Int) =
+//  {
+//    var m = n
+//    do
+//    {
+//      m -= 1
+//      println("Hello World")
+//    }
+//    while (m > 0)
+//  }
+
+  //Second Method (From Discussions)
+//  def f(n: Int) = println("Hello World\n" * n)
+
+
+  //third Method (From Discussions)
+//  def hello = println("Hello World")
+//  def f(n : Int) = 1 to n map (i => hello)
+
+
+  //fourth Method (From Discussions)
+//  def f(n:Int):Unit = {
+//    (1 to n).foreach (_ => println("Hello World"))
+//  }
+
+
+  //Fifth Solution ( My Recursive function)
+  def f(n : Int): Int = n match
     {
-      m -= 1
-      println("Hello World")
-    }
-    while (m > 0)
+    case 0 => 0
+    case _ => println("Hello World")
+      f(n-1)
   }
+
+
 
 var n = scala.io.StdIn.readInt
 
